aa5ac896ad714f62fbb37823b5de9f750688c725
AhmadJaber <ahmadjaberfort@gmail.com>
2026-01-26 15:29:53 +0300

    added titles when hovering over weather information

--- weather-frontend/src/HomePage/HomeTownWeather.jsx
+++ weather-frontend/src/HomePage/HomeTownWeather.jsx
@@ -5,7 +5,7 @@ export default function HomeTownWeather({ homeTownWeather, cityImage }) {
         <div className="home-town-container" style={{backgroundImage: cityImage?`url(${cityImage})`:null}}>
             {homeTownWeather &&
                 <>
-                    <div className="name-and-general">
+                    <div className="name-and-current">
                         <div className="city-country">
                             <i className="fa-solid fa-location-dot fa-xl"></i>
                             <div>
@@ -13,24 +13,28 @@ export default function HomeTownWeather({ homeTownWeather, cityImage }) {
                             </div>
                         </div>
 
-                        <div className="general-weather">
+                        <div className="current-weather">
                             <div className="info">
                                 <img className="state-icon" src={homeTownWeather.forecast.forecastday[0].day.condition.icon} alt="" />
                                 {homeTownWeather.forecast.forecastday[0].day.condition.text}
                             </div>
-                            <div className="info">
+                            <div className="info" title="Temperature">
                                 <i className="fa-solid fa-temperature-half fa-xl"></i>
                                 {homeTownWeather.current.temp_c} &deg;C
                             </div>
-                            <div className="info">
+                            <div className="info" title="Wind speed">
                                 <i className="fa-solid fa-wind fa-xl"></i>
                                 {homeTownWeather.current.wind_kph} km/h
                             </div>
-                            <div className="info">
+                            <div className="info" title="Humidity">
                                 <i className="fa-solid fa-droplet fa-xl"></i>
                                 {homeTownWeather.current.humidity}%
                             </div>
                         </div>
+                    </div>
+
+                    <div className="future-data">
+
                     </div>
                 </>
             }
