f47218b0f219023ae9551382cb747d4b5e75a6d7
AhmadJaber <ahmadjaberfort@gmail.com>
2026-02-08 18:17:32 +0300

    added the footer to th saved page

--- weather-frontend/src/SavedPage/SavedPage.jsx
+++ weather-frontend/src/SavedPage/SavedPage.jsx
@@ -1,7 +1,6 @@
-import axios from 'axios';
-import { useState, useEffect } from "react";
 import Header from "../MainComponents/Header";
 import SavedContainer from './SavedContainer';
+import Footer from "../MainComponents/Footer";
 
 export default function SavedPage({savedCities, handleSave, loading, error}) {
     return (
@@ -10,6 +9,7 @@ export default function SavedPage({savedCities, handleSave, loading, error}) {
 
             <Header />
             <SavedContainer message={loading ? 'Loading...' : error ? 'An Error Occured' : 'Saved Cities'} savedCities={savedCities} handleSave={handleSave} />
+            <Footer/>
         </>
     );
 }
